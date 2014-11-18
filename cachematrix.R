ecd593d063dca462b382538293f0dc6a91fade57
Robert Lothian <sharksmustdie@yahoo.com>
2014-11-18 19:05:21 +0000

    This is the solved assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The programming assignment was to create a pair of functions that carry out a matrix inverse,
+## caching the result to avoid repeating the calculation. The solution below follows the 
+## pattern of the vector mean example very closely.
 
-## Write a short comment describing this function
+## This function creates a list of setters and getters for the matrix
+## and its inverse. It is just a matrix version of makeVector.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse.in) inverse <<- inverse.in
+        getinverse <- function() inverse
+        list(set=set, get = get, setinverse = setinverse, getinverse = getinverse)    
 }
 
 
-## Write a short comment describing this function
+## This function checks if the inverse has been cached and uses
+## the result if it has. Otherwise, it carries out the inverse by calling solve.
+## It is very similar to cachemean.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached inverse")
+                return(inverse)
+        }
+        mat.temp <- x$get()
+        inverse <- solve(mat.temp, ...)
+        x$setinverse(inverse)
+        inverse
 }
